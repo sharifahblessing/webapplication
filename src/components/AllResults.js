4290472f743c0be04ab9ee5a9e9e8c06a3dc0c3d
Naluswata <snaluswata18@gmail.com>
2019-08-09 17:33:22 +0300

    create result and all results components

---
+++ src/components/AllResults.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class AllResult extends  React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Results</h1>
+                <div className="resultsbar">
+                    
+                </div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default AllResult;
---
+++ src/components/result.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Result extends  React.Component{
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default Result;
